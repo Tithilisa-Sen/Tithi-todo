88dfcbcc109e2a54969cb327916bb202e2c21244
Tithilisa-Sen <sentithilisa@gmail.com>
2024-12-03 11:16:10 +0530

    first commit 4

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -47,4 +47,13 @@ router.patch("/:id",async(req,res)=>{
     }
 });
 
+router.delete("/:id",async(req,res)=>{
+    try{
+      const deletedList=await List.findByIdAndDelete(req.params.id);
+      res.status(200).json(deletedList);
+    }catch(error){
+    res.status(400).send("Error: "+error);
+    }
+});
+
 module.exports=router;
